11a779c4357c068d9fca36944f80c68df855b389
Scott R. Parish <sparish@peak6.com>
2012-01-31 12:50:33 -0600

    pub_service: don't forward on any close messages
    This hasn't been working right, and i can't think of a good reason
    why we really need to support this anyway.

--- src/pub_service.erl
+++ src/pub_service.erl
@@ -67,12 +67,7 @@ handle_call({mmd, From, Msg}, _From, State=#state{chans=Chans, sub=Sub}) ->
 	    {NewChans, #channel_message{body=Body}} ->
 		sub_service:pub(Sub, Body),
 		NewChans;
-	    {NewChans, #channel_close{body = <<?NULL>>}} ->
-		NewChans;
-	    {NewChans, #channel_close{body = ?error(_, _)}} ->
-		NewChans;
-	    {NewChans, #channel_close{body=Body}} ->
-		sub_service:pub(Sub, Body),
+	    {NewChans, #channel_close{}} ->
 		NewChans
 	end,
     {reply, ok, State#state{chans=NewChans3}};
