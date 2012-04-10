8f9a9617c2648f1bd7834b9f4ac5eb6cf17cfd18
Scott R. Parish <sparish@peak6.com>
2012-04-10 12:50:19 -0500

    channel_mgr: use records for ets entries

--- src/channel_mgr.erl
+++ src/channel_mgr.erl
@@ -23,12 +23,14 @@
 -define(tid(S), S#state.tid).
 -define(max_chans(S), S#state.max_chans).
 
+-record(chan, {id, remote}).
+
 new() -> new(?MAX_CONCURRENT_CHANNELS).
-new(MaxChans) -> #state{tid = ets:new(channel_mgr, [set]),
+new(MaxChans) -> #state{tid = ets:new(channel_mgr, [set, {keypos, 2}]),
 			max_chans = MaxChans}.
 
 sendAll(State, Body) ->
-    ets:foldl(fun({Id, Ref}, S) ->
+    ets:foldl(fun(#chan{id = Id, remote = Ref}, S) ->
 		      fire(Ref,#channel_message{id=Id,body=Body}),
 		      S
 	      end, State, ?tid(State)).
@@ -45,21 +47,21 @@ processOut(State, M=#channel_create{id=Id}, Cfg) ->
 	true -> {State, maxChans(Id, ?max_chans(State))};
 	false ->
 	    {ok,Pid} = client_channel:new(self(),M,Cfg),
-	    case ets:insert_new(?tid(State), {Id, Pid}) of
+	    case ets:insert_new(?tid(State), #chan{id = Id, remote = Pid}) of
 		true -> {State, []};
 		false -> {State, dupId(Id)}
 	    end
     end;
 
 processOut(State, M=#channel_message{id=Id},_Cfg) ->
-    case ets:lookup_element(?tid(State), Id, 2) of
+    case ets:lookup_element(?tid(State), Id, 3) of
         badarg -> {State, noSuchChannel(Id)};
         Pid -> fire(Pid,M),
 	       {State, []}
     end;
 
 processOut(State,M=#channel_close{id=Id},_Cfg) ->
-    case ets:lookup_element(?tid(State), Id, 2) of
+    case ets:lookup_element(?tid(State), Id, 3) of
         badarg ->
             ?lwarn("Attempt to close unknown channel: ~p",[Id]),
             {State, noSuchChannel(Id)};
@@ -69,7 +71,7 @@ processOut(State,M=#channel_close{id=Id},_Cfg) ->
     end.
 
 processIn(State, From,M=#channel_create{id=Id}) ->
-    case ets:insert_new(?tid(State), {Id, From}) of
+    case ets:insert_new(?tid(State), #chan{id = Id, remote = From}) of
         true -> {State, M};
         false -> dupId(From,Id),
                 State
@@ -90,10 +92,11 @@ fire(To,Msg) -> fire(self(),To,Msg).
 fire(From,To,Msg) -> To ! {mmd,From,Msg}.
 
 refToIds(State, Ref) ->
-    ets:select(?tid(State), [{{'$1', Ref}, [], ['$1']}]).
+    ets:select(?tid(State),
+	       [{#chan{id = '$1', remote = Ref, _ = '_'}, [], ['$1']}]).
 
 removeRef(State, Ref) ->
-    ets:match_delete(?tid(State), [{'_', Ref}]),
+    ets:match_delete(?tid(State), [#chan{remote = Ref, _ = '_'}]),
     State.
 
 maxChans(Id, MaxChans) ->
