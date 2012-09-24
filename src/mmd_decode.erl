cd103871fbac10c29f94ae4605a3529abb893cd5
Scott R. Parish <sparish@peak6.com>
2012-09-24 09:35:51 -0500

    mmd_decode: reorder decode_obj to preference v1_1 and expected freq of types

--- src/mmd_decode.erl
+++ src/mmd_decode.erl
@@ -64,61 +64,17 @@ decode_head(<<?CHANNEL_MESSAGE, Chan:16/binary, Body/binary>>) ->
 decode_head(<<?CHANNEL_CLOSE, Chan:16/binary, Body/binary>>) ->
     #channel_close{id=Chan, body=?raw(Body)}.
 
-decode_obj(M=#channel_create{}) -> M;
-decode_obj(M=#channel_message{}) -> M;
-decode_obj(M=#channel_close{}) -> M;
-decode_obj(?TAG_WITH_SIZE(?FAST_BYTES,Sz,Rest)) ->
-    <<Bytes:Sz/binary,Rem/binary>> = Rest,
-    {?bytes(Bytes),Rem};
-decode_obj(<<?FAST_ERROR,Rest1/binary>>) ->
-    {Code, Rest2} = decode_obj(Rest1),
-    {Obj, Rest3} = decode_obj(Rest2),
-    {?error(Code, Obj), Rest3};
-decode_obj(<<?VARINT_ERROR, Bin/binary>>) ->
-    {ErrCode, PostErr} = decode_svarint(Bin),
-    {Msg, Rest} = decode_obj(PostErr),
-    {?error(ErrCode, Msg), Rest};
-decode_obj(<<?VARINT_ARRAY,Bin/binary>>) ->
-    {Sz,Rest} = decode_varint(Bin),
-    decode_array(Rest,Sz,[]);
-decode_obj(?TAG_WITH_SIZE(?FAST_ARRAY,Sz,Rest)) ->
-    decode_array(Rest,Sz,[]);
 decode_obj(?TAG_WITH_SIZE(?FAST_STRING,Sz,Rest)) ->
     <<Str:Sz/binary,Rest2/binary>> = Rest,
     {Str,Rest2};
-decode_obj(?TAG_WITH_SIZE(?FAST_MAP,Sz,Rest)) ->
-    decode_map(Rest,Sz,[]);
-decode_obj(<<?FAST_TIME,Time:64/integer,Rest/binary>>) ->
-    {?time(Time),Rest};
-decode_obj(<<?VARINT_MAP,Bin/binary>>) ->
-    {Sz,Data} = decode_varint(Bin),
-    decode_map(Data,Sz,[]);
-decode_obj(<<?UUID, UUID:16/binary, Rest/binary>>) ->
-    {?uuid(list_to_binary(uuid:to_string(UUID))), Rest};
-decode_obj(<<?SECID, SecID:16/binary, Rest/binary>>) ->
-    {?secid(SecID), Rest};
-decode_obj(<<?VARINT_BYTES, Data/binary>>) ->
-    {Sz, PostSz} = decode_varint(Data),
-    <<Bytes:Sz/binary, Rest/binary>> = PostSz,
-    {?bytes(Bytes), Rest};
-decode_obj(<<?BYTE, B, Rest/binary>>) ->
-    {?byte(B),Rest};
-decode_obj(<<?VARINT_STRING,Data/binary>>) ->
-    {Sz, PostSz} = decode_varint(Data),
-    <<Str:Sz/binary, Rest/binary>> = PostSz,
-    {Str,Rest};
 decode_obj(<<?INT0, Data/binary>>) ->
     {0, Data};
 decode_obj(<<?UINT0, Data/binary>>) ->
     {0, Data};
-decode_obj(<<?SVARINT64, Data/binary>>) ->
-    decode_svarint(Data);
-decode_obj(<<?VARINT64, Data/binary>>) ->
-    decode_svarint(Data);
-decode_obj(<<?SVARINT32, Data/binary>>) ->
-    decode_svarint(Data);
-decode_obj(<<?VARINT32, Data/binary>>) ->
-    decode_varint(Data);
+decode_obj(<<1:4,Sz:4,Int:Sz/unsigned-unit:8,Rest/binary>>) ->
+    {Int,Rest};
+decode_obj(<<0:4,Sz:4,Int:Sz/signed-unit:8,Rest/binary>>) ->
+    {Int,Rest};
 decode_obj(<<?DOUBLE, Double:64/float, Rest/binary>>) ->
     {Double, Rest};
 decode_obj(<<?FLOAT, Float:32/float, Rest/binary>>) ->
@@ -129,13 +85,57 @@ decode_obj(<<?TRUE,Data/binary>>) ->
     {true, Data};
 decode_obj(<<?FALSE,Data/binary>>) ->
     {false, Data};
+decode_obj(?TAG_WITH_SIZE(?FAST_BYTES,Sz,Rest)) ->
+    <<Bytes:Sz/binary,Rem/binary>> = Rest,
+    {?bytes(Bytes),Rem};
+decode_obj(<<?FAST_TIME,Time:64/integer,Rest/binary>>) ->
+    {?time(Time),Rest};
+decode_obj(<<?UUID, UUID:16/binary, Rest/binary>>) ->
+    {?uuid(list_to_binary(uuid:to_string(UUID))), Rest};
+decode_obj(<<?SECID, SecID:16/binary, Rest/binary>>) ->
+    {?secid(SecID), Rest};
+decode_obj(<<?BYTE, B, Rest/binary>>) ->
+    {?byte(B),Rest};
+decode_obj(?TAG_WITH_SIZE(?FAST_ARRAY,Sz,Rest)) ->
+    decode_array(Rest,Sz,[]);
+decode_obj(?TAG_WITH_SIZE(?FAST_MAP,Sz,Rest)) ->
+    decode_map(Rest,Sz,[]);
+decode_obj(<<?FAST_ERROR,Rest1/binary>>) ->
+    {Code, Rest2} = decode_obj(Rest1),
+    {Obj, Rest3} = decode_obj(Rest2),
+    {?error(Code, Obj), Rest3};
+decode_obj(M=#channel_create{}) -> M;
+decode_obj(M=#channel_message{}) -> M;
+decode_obj(M=#channel_close{}) -> M;
+decode_obj(<<?VARINT_STRING,Data/binary>>) ->
+    {Sz, PostSz} = decode_varint(Data),
+    <<Str:Sz/binary, Rest/binary>> = PostSz,
+    {Str,Rest};
+decode_obj(<<?SVARINT64, Data/binary>>) ->
+    decode_svarint(Data);
+decode_obj(<<?VARINT64, Data/binary>>) ->
+    decode_svarint(Data);
+decode_obj(<<?SVARINT32, Data/binary>>) ->
+    decode_svarint(Data);
+decode_obj(<<?VARINT32, Data/binary>>) ->
+    decode_varint(Data);
 decode_obj(<<?VARINT_TIME,Data/binary>>) ->
     {Ts, Rest} = decode_svarint(Data),
     {?time(Ts), Rest};
-decode_obj(<<1:4,Sz:4,Int:Sz/unsigned-unit:8,Rest/binary>>) ->
-    {Int,Rest};
-decode_obj(<<0:4,Sz:4,Int:Sz/signed-unit:8,Rest/binary>>) ->
-    {Int,Rest};
+decode_obj(<<?VARINT_ARRAY,Bin/binary>>) ->
+    {Sz,Rest} = decode_varint(Bin),
+    decode_array(Rest,Sz,[]);
+decode_obj(<<?VARINT_MAP,Bin/binary>>) ->
+    {Sz,Data} = decode_varint(Bin),
+    decode_map(Data,Sz,[]);
+decode_obj(<<?VARINT_BYTES, Data/binary>>) ->
+    {Sz, PostSz} = decode_varint(Data),
+    <<Bytes:Sz/binary, Rest/binary>> = PostSz,
+    {?bytes(Bytes), Rest};
+decode_obj(<<?VARINT_ERROR, Bin/binary>>) ->
+    {ErrCode, PostErr} = decode_svarint(Bin),
+    {Msg, Rest} = decode_obj(PostErr),
+    {?error(ErrCode, Msg), Rest};
 decode_obj(<<Other:1/binary,_Rest/binary>>) ->
     throw({bad_tag,Other}).
 
