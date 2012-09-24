ad7f741ad4d5bc7b5c08792d1935b0b8ffffc7a8
Scott R. Parish <sparish@peak6.com>
2012-09-24 09:29:26 -0500

    mmd_decode: inline decode_type into decode_obj
    Nobody's calling decode_type and it makes the code more complex
    keeping them separate.

--- src/mmd_decode.erl
+++ src/mmd_decode.erl
@@ -45,17 +45,19 @@ decode_head(<<?CHANNEL_CREATE, Chan:16/binary, Type:1/binary,
                     timeout=if Timeout < 0 -> Timeout * -1; true -> Timeout*1000 end,
                     auth_token=AT,
                     id=binary:copy(Chan),
-                    body=?raw(Body)};
+                    body=Body};
+                    %body=?raw(upgrade_body(Vsn, Body))};
 
-decode_head(<<?VARINT_CHANNEL_CREATE, Chan:16/binary, Data/binary>>) ->
-    {[Type, Svc, Timeout, AT], Body} =
-        decode_chain(Data, [channel_type, atom, svarint64, uuid_notag]),
-    #channel_create{service=Svc,
-                    type=Type,
+decode_head(<<?VARINT_CHANNEL_CREATE, Chan:16/binary, Type, Data/binary>>) ->
+    {Sz, Data2} = decode_varint(Data),
+    <<Svc:Sz/binary, Data3/binary>> = Data2,
+    {Timeout, <<AT:16/binary, Data4/binary>>} = decode_varint(Data3),
+    #channel_create{service=binary_to_atom(Svc, utf8),
+                    type=channel_type(Type),
                     timeout=Timeout,
                     auth_token=AT,
                     id=binary:copy(Chan),
-                    body=?raw(Body)};
+                    body=?raw(Data4)};
 
 decode_head(<<?CHANNEL_MESSAGE, Chan:16/binary, Body/binary>>) ->
     #channel_message{id=Chan, body=?raw(Body)};
@@ -69,13 +71,13 @@ decode_obj(?TAG_WITH_SIZE(?FAST_BYTES,Sz,Rest)) ->
     <<Bytes:Sz/binary,Rem/binary>> = Rest,
     {?bytes(Bytes),Rem};
 decode_obj(<<?FAST_ERROR,Rest1/binary>>) ->
-    {Code,Rest2} = decode_type(int,Rest1),
-    {Obj,Rest3} = decode_obj(Rest2),
-    {?error(Code,Obj),Rest3};
-decode_obj(<<?VARINT_ERROR,Bin/binary>>) ->
-    {ErrCode,PostErr} = decode_type(svarint64,Bin),
-    {Msg,Rest} = decode_obj(PostErr),
-    {?error(ErrCode,Msg),Rest};
+    {Code, Rest2} = decode_obj(Rest1),
+    {Obj, Rest3} = decode_obj(Rest2),
+    {?error(Code, Obj), Rest3};
+decode_obj(<<?VARINT_ERROR, Bin/binary>>) ->
+    {ErrCode, PostErr} = decode_svarint(Bin),
+    {Msg, Rest} = decode_obj(PostErr),
+    {?error(ErrCode, Msg), Rest};
 decode_obj(<<?VARINT_ARRAY,Bin/binary>>) ->
     {Sz,Rest} = decode_varint(Bin),
     decode_array(Rest,Sz,[]);
@@ -91,32 +93,36 @@ decode_obj(<<?FAST_TIME,Time:64/integer,Rest/binary>>) ->
 decode_obj(<<?VARINT_MAP,Bin/binary>>) ->
     {Sz,Data} = decode_varint(Bin),
     decode_map(Data,Sz,[]);
-decode_obj(<<?UUID,Data/binary>>) ->
-    decode_type(uuid,Data);
-decode_obj(<<?SECID,Data/binary>>) ->
-    decode_type(secid,Data);
-decode_obj(<<?VARINT_BYTES,Data/binary>>) ->
-    decode_type(bytes,Data);
-decode_obj(<<?BYTE,Data/binary>>) ->
-    decode_type(byte,Data);
+decode_obj(<<?UUID, UUID:16/binary, Rest/binary>>) ->
+    {?uuid(list_to_binary(uuid:to_string(UUID))), Rest};
+decode_obj(<<?SECID, SecID:16/binary, Rest/binary>>) ->
+    {?secid(SecID), Rest};
+decode_obj(<<?VARINT_BYTES, Data/binary>>) ->
+    {Sz, PostSz} = decode_varint(Data),
+    <<Bytes:Sz/binary, Rest/binary>> = PostSz,
+    {?bytes(Bytes), Rest};
+decode_obj(<<?BYTE, B, Rest/binary>>) ->
+    {?byte(B),Rest};
 decode_obj(<<?VARINT_STRING,Data/binary>>) ->
-    decode_type(string,Data);
+    {Sz, PostSz} = decode_varint(Data),
+    <<Str:Sz/binary, Rest/binary>> = PostSz,
+    {Str,Rest};
 decode_obj(<<?INT0, Data/binary>>) ->
     {0, Data};
 decode_obj(<<?UINT0, Data/binary>>) ->
     {0, Data};
-decode_obj(<<?SVARINT64,Data/binary>>) ->
-    decode_type(svarint64,Data);
-decode_obj(<<?VARINT64,Data/binary>>) ->
-    decode_type(varint64,Data);
-decode_obj(<<?SVARINT32,Data/binary>>) ->
-    decode_type(svarint32,Data);
-decode_obj(<<?VARINT32,Data/binary>>) ->
-    decode_type(varint32,Data);
-decode_obj(<<?DOUBLE,Data/binary>>) ->
-    decode_type(double,Data);
-decode_obj(<<?FLOAT,Data/binary>>) ->
-    decode_type(float,Data);
+decode_obj(<<?SVARINT64, Data/binary>>) ->
+    decode_svarint(Data);
+decode_obj(<<?VARINT64, Data/binary>>) ->
+    decode_svarint(Data);
+decode_obj(<<?SVARINT32, Data/binary>>) ->
+    decode_svarint(Data);
+decode_obj(<<?VARINT32, Data/binary>>) ->
+    decode_varint(Data);
+decode_obj(<<?DOUBLE, Double:64/float, Rest/binary>>) ->
+    {Double, Rest};
+decode_obj(<<?FLOAT, Float:32/float, Rest/binary>>) ->
+    {Float, Rest};
 decode_obj(<<?NULL,Data/binary>>) ->
     {undefined, Data};
 decode_obj(<<?TRUE,Data/binary>>) ->
@@ -124,77 +130,14 @@ decode_obj(<<?TRUE,Data/binary>>) ->
 decode_obj(<<?FALSE,Data/binary>>) ->
     {false, Data};
 decode_obj(<<?VARINT_TIME,Data/binary>>) ->
-    decode_type(time,Data);
+    {Ts, Rest} = decode_svarint(Data),
+    {?time(Ts), Rest};
 decode_obj(<<1:4,Sz:4,Int:Sz/unsigned-unit:8,Rest/binary>>) ->
     {Int,Rest};
 decode_obj(<<0:4,Sz:4,Int:Sz/signed-unit:8,Rest/binary>>) ->
     {Int,Rest};
 decode_obj(<<Other:1/binary,_Rest/binary>>) ->
     throw({bad_tag,Other}).
-
-
-
-
-
-
-decode_chain(Bin,Types) -> decode_chain(Bin,Types,[]).
-decode_chain(Bin,[],Acc) -> {lists:reverse(Acc),Bin};
-decode_chain(Bin,[Type|Types],Acc) ->
-    {Data,NewBin} = decode_type(Type,Bin),
-    decode_chain(NewBin,Types,[Data|Acc]).
-
-decode_type(channel_type,<<B:1/binary,Rest/binary>>) ->
-    case B of
-        <<"C">> -> {call,Rest};
-        <<"S">> -> {sub,Rest};
-        Other -> {Other,Rest}  %% if we can't decode, leave in byte form
-    end;
-decode_type(exatom,Bin) ->
-    {Str,Rest} = decode_type(string,Bin),
-    Atom = try binary_to_existing_atom(Str,utf8)
-           catch Type:Reason -> ?lwarn("Failed to convert: ~p in to existing atom {~p,~p}",[Str,Type,Reason]), Str
-           end,
-    {Atom,Rest};
-decode_type(atom,Bin) ->
-    {Str,Rest} = decode_type(string,Bin),
-    {binary_to_atom(Str,utf8),Rest};
-decode_type(uuid,<<UUID:16/binary,Rest/binary>>) ->
-    {?uuid(list_to_binary(uuid:to_string(UUID))), Rest};
-decode_type(uuid_notag,<<UUID:16/binary,Rest/binary>>) ->
-    {UUID,Rest};
-decode_type(secid,<<SecID:16/binary,Rest/binary>>) ->
-    {?secid(SecID),Rest};
-decode_type(bytes,Bin) ->
-    {Sz,PostSz} = decode_varint(Bin),
-    <<Bytes:Sz/binary,Rest/binary>> = PostSz,
-    {?bytes(Bytes),Rest};
-decode_type(byte,<<B,Rest/binary>>) ->
-    {?byte(B),Rest};
-decode_type(string,Bin) ->
-    {Sz,PostSz} = decode_varint(Bin),
-    <<Str:Sz/binary,Rest/binary>> = PostSz,
-    {Str,Rest};
-decode_type(int,<<1:4,Sz:4,Int:Sz/unsigned-unit:8,Rest/binary>>) ->
-    {Int,Rest};
-decode_type(int,<<0:4,Sz:4,Int:Sz/signed-unit:8,Rest/binary>>) ->
-    {Int,Rest};
-decode_type(svarint32,Bin) ->
-    decode_svarint(Bin);
-decode_type(svarint64,Bin) ->
-    decode_svarint(Bin);
-decode_type(varint32,Bin) ->
-    decode_varint(Bin);
-decode_type(varint64,Bin) ->
-    decode_varint(Bin);
-decode_type(double,<<Double:64/float,Rest/binary>>) ->
-    {Double,Rest};
-decode_type(float,<<Float:32/float,Rest/binary>>) ->
-    {Float,Rest};
-decode_type(time,Bin) ->
-    {Ts,Rest} = decode_svarint(Bin),
-    {?time(Ts),Rest}.
-
-
 
 decode_map(Bin,0,Acc) ->
     {?map(lists:reverse(Acc)),Bin};
@@ -211,7 +154,6 @@ decode_array(Bin,Sz,Acc) ->
     {E,Rem} = decode_obj(Bin),
     decode_array(Rem,Sz-1,[E|Acc]).
 
-
 decode_svarint(Bytes) ->
     {Value,Rest} = decode_varint(Bytes, 0, 0),
     {(Value bsr 1) bxor (-(Value band 1)),Rest}.
@@ -226,6 +168,5 @@ decode_varint(<<1:1, I:7, Rest/binary>>, Acc, Shift) ->
 
 channel_type(<<"C">>) -> call;
 channel_type(<<"S">>) -> sub.
-
 
 %% vim: ts=4:sts=4:sw=4:et:sta:
