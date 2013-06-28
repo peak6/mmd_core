4845fffcf4fd2f5e75ad9112aec9d44047bce6c7
Rob Wisniewski <rwisniewski@peak6.com>
2013-06-28 11:25:06 -0500

    remove decode logging

--- src/json_decode.erl
+++ src/json_decode.erl
@@ -29,12 +29,8 @@ decodeObj(JS) ->
 
 decode({json,Json}) -> decode(Json);
 decode(Json) ->
-    ?ldebug("json: ~p~n", [Json]),
     {ok,Obj,[]} = json:decode(Json),
-    ?ldebug("obj: ~p~n", [Obj]),
-    DecodeObj = decodeDoc(Obj),
-    ?ldebug("decodeobj: ~p~n", [DecodeObj]),
-    DecodeObj.
+    catch decodeDoc(Obj).
 
 decodeDoc({obj,Map}) ->
     case p6props:first(["call","sub","close","msg"],Map) of
