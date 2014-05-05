9c06ded8013152dea47ec1f962ceb26aaa06524b
David Budworth <dbudworth@peak6.com>
2014-05-05 13:54:12 -0500

    added support for channel close fast path

--- src/json_encode.erl
+++ src/json_encode.erl
@@ -21,9 +21,12 @@
 encode(?raw(Bin)) -> encode(mmd_decode:decodeRawFull(Bin));
 
 encode(CM=#channel_message{body=?raw(Data)}) -> encode(CM#channel_message{body=mmd_decode:decodeRawFull(Data)});
+encode(CM=#channel_close{body=?raw(Data)}) -> encode(CM#channel_close{body=mmd_decode:decodeRawFull(Data)});
     
 encode(#channel_message{id=Id,body=?map([{<<"json">>,JSON}])}) when is_binary(JSON) ->
     {ok,[${,<<"\"msg\"">>,$:,?QUOTE,uuidToStr(Id),?QUOTE,$,,<<"\"body\"">>,$:,JSON,$}]};
+encode(#channel_close{id=Id,body=?map([{<<"json">>,JSON}])}) when is_binary(JSON) ->
+    {ok,[${,<<"\"close\"">>,$:,?QUOTE,uuidToStr(Id),?QUOTE,$,,<<"\"body\"">>,$:,JSON,$}]};
 
 encode(Data) -> {ok,json:encode(encode_obj(Data))}.
 
